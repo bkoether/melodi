2c8e1147247b6cf9232a1080db3097a7e6642ae4
Benjamin Koether <benjamin@imagexmedia.com>
2010-11-07 15:22:56 -0800

    EOD changes

--- sites/default/modules/melodi/melodi.js
+++ sites/default/modules/melodi/melodi.js
@@ -4,3 +4,13 @@ Drupal.behaviors.cleanDD = function (context) {
     $(this).text(str.replace(/\&amp;/gi, "&"));
   });
 };
+
+Drupal.behaviors.colorTable = function (context) {
+  $('.view-master-list table', context).each(function () {
+    var label = $(this).find('caption').text();
+    label = label.toLowerCase();
+    label = label.replace(/\&/gi, "");
+    label = label.replace(/ /gi, "-");
+    $(this).addClass(label);
+  });
+};
